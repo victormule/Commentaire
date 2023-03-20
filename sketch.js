3417354d0d2272026e2bebb62720d29f2fefca99
victormule <50982984+victormule@users.noreply.github.com>
2023-03-21 00:15:55 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,36 @@
+// Variables pour stocker la position de la souris
+let mouseXPos;
+let mouseYPos;
+
 function setup() {
-  createCanvas(400, 200);
+	// Créer le canvas P5.js
+	createCanvas(400, 200).parent('canvas-container');
+	// Ajouter un événement de souris pour détecter la position de la souris
+	canvas.addEventListener('mousemove', function(event) {
+		mouseXPos = event.clientX;
+		mouseYPos = event.clientY;
+	});
+	// Initialiser CommentBox.io
+	commentBox('5633336189911040-proj', {
+		createBoxUrl(boxId) {
+			// Renvoyer l'URL de la page actuelle avec l'ID de la boîte ajouté à la fin
+			return window.location.href + '#' + boxId;
+		},
+	}).then(function(commentBox) {
+		// Ajouter un événement de souris pour afficher la boîte de commentaires
+		canvas.addEventListener('mousemove', function() {
+			if (mouseXPos <= 400 && mouseYPos <= 200) {
+				commentBox.show();
+			} else {
+				commentBox.hide();
+			}
+		});
+	});
 }
 
 function draw() {
-  background(220);
-  ellipse(mouseX, mouseY, 50, 50);
+	// Dessiner quelque chose sur le canvas P5.js
+	background(220);
+	fill(255, 0, 0);
+	ellipse(200, 100, 50, 50);
 }
